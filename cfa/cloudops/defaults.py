148c0c8d414b4182d3b796ccb3c4e1730eec5e71
Ryan Raasch <xng3@cdc.gov>
2025-07-14 20:54:32 +0000

    fix circular import

--- cfa/cloudops/defaults.py
+++ cfa/cloudops/defaults.py
@@ -4,7 +4,55 @@ Default configurations for Azure resources.
 
 from azure.mgmt.batch import models
 
-from .util import remaining_task_autoscale_formula
+
+# autoscale default formula
+def remaining_task_autoscale_formula(
+    task_sample_interval_minutes: int = 15,
+    max_number_vms: int = 10,
+):
+    """
+    Get an autoscaling formula that rescales pools based on the remaining task count.
+
+    Parameters
+    ----------
+    task_sample_interval_minutes
+        Task sampling interval, in minutes, as an integer.
+        Default 15.
+
+    max_number_vms
+        Maximum number of virtual machines to spin
+        up, regardless of the number of remaining
+        tasks. Default 10.
+
+    Returns
+    -------
+    str
+        The autoscale formula, as a string.
+    """
+    autoscale_formula_template = """// In this example, the pool size
+    // is adjusted based on the number of tasks in the queue.
+    // Note that both comments and line breaks are acceptable in formula strings.
+
+    // Get pending tasks for the past 15 minutes.
+    $samples = $ActiveTasks.GetSamplePercent(TimeInterval_Minute * {task_sample_interval_minutes});
+    // If we have fewer than 70 percent data points, we use the last sample point, otherwise we use the maximum of last sample point and the history average.
+    $tasks = $samples < 70 ? max(0, $ActiveTasks.GetSample(1)) :
+    max( $ActiveTasks.GetSample(1), avg($ActiveTasks.GetSample(TimeInterval_Minute * {task_sample_interval_minutes})));
+    // If number of pending tasks is not 0, set targetVM to pending tasks, otherwise half of current dedicated.
+    $targetVMs = $tasks > 0 ? $tasks : max(0, $TargetDedicatedNodes / 2);
+    // The pool size is capped at {max_number_vms}, if target VM value is more than that, set it to {max_number_vms}.
+    cappedPoolSize = {max_number_vms};
+    $TargetDedicatedNodes = max(0, min($targetVMs, cappedPoolSize));
+    // Set node deallocation mode - keep nodes active only until tasks finish
+    $NodeDeallocationOption = taskcompletion;"""
+
+    autoscale_formula = autoscale_formula_template.format(
+        task_sample_interval_minutes=task_sample_interval_minutes,
+        max_number_vms=max_number_vms,
+    )
+
+    return autoscale_formula
+
 
 # image defaults
 default_image_publisher = "microsoft-dsvm"
