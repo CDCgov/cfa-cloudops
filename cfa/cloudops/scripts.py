fdd4cfdc089929379c0f69429c86b2ba1c07118c
Ryan Raasch <xng3@cdc.gov>
2025-09-04 20:16:44 +0000

    fix args

--- cfa/cloudops/scripts.py
+++ cfa/cloudops/scripts.py
@@ -3,7 +3,7 @@ import argparse
 # from cfa.cloudops import CloudClient
 
 
-def hello(name: str):
+def hello():
     parser = argparse.ArgumentParser(description="CloudOps parser")
     parser.add_argument(
         "--name", type=str, default="World", help="Name to greet"
