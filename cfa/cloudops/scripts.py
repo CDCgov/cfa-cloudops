18c237f7d007818ba09fa91b8eda2ef98c0254bc
Ryan Raasch <xng3@cdc.gov>
2025-09-04 20:13:47 +0000

    add args

--- cfa/cloudops/scripts.py
+++ cfa/cloudops/scripts.py
@@ -1,4 +1,12 @@
-from cfa.cloudops import CloudClient
+import argparse
 
-def hello():
-    print("Hello, CloudOps!")
+# from cfa.cloudops import CloudClient
+
+
+def hello(name: str):
+    parser = argparse.ArgumentParser(description="CloudOps parser")
+    parser.add_argument(
+        "--name", type=str, default="World", help="Name to greet"
+    )
+    args = parser.parse_args()
+    print(f"Hello, {args.name}!")
