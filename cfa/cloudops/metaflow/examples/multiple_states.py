c5a499f7804f0edfb3c8a9613de45a3a7e503492
Fawad Rafi <xop5@cdc.gov>
2025-09-05 10:17:04 -0400

    Linting issues

--- cfa/cloudops/metaflow/examples/multiple_states.py
+++ cfa/cloudops/metaflow/examples/multiple_states.py
@@ -1,7 +1,11 @@
 import logging
+from custom_metaflow.plugins.decorators.cfa_azure_batch_decorator import (
+    CFAAzureBatchDecorator
+)
+from custom_metaflow.cfa_batch_pool_service import ( 
+    CFABatchPoolService
+)
 from metaflow import FlowSpec, step
-from custom_metaflow.plugins.decorators.cfa_azure_batch_decorator import CFAAzureBatchDecorator
-from custom_metaflow.cfa_batch_pool_service import CFABatchPoolService
 
 logger = logging.getLogger(__name__)
 
@@ -12,9 +16,13 @@ class MyFlow(FlowSpec):
         self.all_states = []
         with open('states.txt', 'r') as file:
             all_states = file.read().splitlines()
-        self.batch_pool_service = CFABatchPoolService(dotenv_path='metaflow.env')
+        self.batch_pool_service = CFABatchPoolService(
+            dotenv_path='metaflow.env'
+        )
         self.batch_pool_service.setup_pools()
-        self.split_lists = self.batch_pool_service.setup_step_parameters(all_states, "job.toml")
+        self.split_lists = self.batch_pool_service.setup_step_parameters(
+            all_states, "job.toml"
+        )
         self.next(self.process_state, foreach='split_lists')
         
     @step
@@ -32,7 +40,9 @@ class MyFlow(FlowSpec):
     def _process_state(self):
         step_pool_name = self.input['pool_name']
         step_parameters = self.input['task_parameters']
-        logger.info(f"Running the _process_state step in Azure Batch for pool {step_pool_name} with {len(step_parameters)} parameters which are {step_parameters}")
+        logger.info(
+            f"Running the _process_state step in Azure Batch for pool {step_pool_name} with {len(step_parameters)} parameters which are {step_parameters}"
+        )
 
     @step
     def join(self, inputs):
