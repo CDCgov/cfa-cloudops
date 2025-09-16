d1c1d8b3ca1583b331aefa485d2e922848f67b99
Fawad Rafi <xop5@cdc.gov>
2025-09-16 03:15:33 +0000

    Split the input and move all files to subfolder

--- cfa/cloudops/metaflow/examples/bk_algorithm.py
+++ cfa/cloudops/metaflow/examples/bk_algorithm.py
@@ -1,22 +1,11 @@
 import argparse
 import logging
 import random
-import string
 
 import matplotlib.pyplot as plt
 import networkx as nx
 
-
-def generate_random_string(length):
-    """Generates a random string of specified length using letters and digits."""
-    characters = string.ascii_letters + string.digits
-    random_string = "".join(random.choice(characters) for _ in range(length))
-    return random_string
-
-
-SUFFIX = generate_random_string(5)
-
-fh = logging.FileHandler(f"cliques/clique_app_{SUFFIX}.log")
+fh = logging.FileHandler("cliques/clique_app.log", mode="a")
 fh.setLevel(logging.INFO)
 
 logger = logging.getLogger(__name__)
@@ -26,20 +15,20 @@ parser = argparse.ArgumentParser()
 parser.add_argument(
     "-n",
     "--nodes",
-    type=int,
+    type=str,
     help="number of nodes",
 )
 
 
-def generate_random_graph(total_nodes):
+def generate_random_graph(node_count):
     edge_probability = random.uniform(0.01, 0.99)
     return (
-        nx.erdos_renyi_graph(n=total_nodes, p=edge_probability),
+        nx.erdos_renyi_graph(n=node_count, p=edge_probability),
         edge_probability,
     )
 
 
-def generate_cliques(random_graph, edge_probability):
+def generate_cliques(random_graph):
     maximal_cliques_iterator = nx.find_cliques(random_graph)
     maximal_cliques = list(maximal_cliques_iterator)
     return sorted(maximal_cliques, key=lambda item: len(item), reverse=True)
@@ -55,24 +44,29 @@ def save_statistics(sorted_cliques):
     return statistics_msgs
 
 
-def save_plot(random_graph, plot_caption):
+def save_plot(random_graph, n_index, plot_caption):
     plt.figure(figsize=(10, 10))
     nx.draw(random_graph, node_size=10, width=0.1)
     plt.suptitle(plot_caption)
-    plt.savefig(f"cliques/my_plot_{SUFFIX}.png")
+    plt.savefig(f"cliques/my_plot_{n_index}.png")
 
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    total_nodes = args.nodes if args.nodes else 100
-    random_graph, edge_probability = generate_random_graph(total_nodes)
-    plot_title = f"Generated an Erdős-Rényi graph with {total_nodes} nodes and edge probability {edge_probability}."
-    logger.info(plot_title)
-    sorted_cliques = generate_cliques(random_graph, edge_probability)
-    clique_count_msg = (
-        f"Found {len(sorted_cliques)} maximal cliques."
-        + save_statistics(sorted_cliques)
-    )
-    logger.info(clique_count_msg)
-    plot_caption = "\n".join([plot_title, clique_count_msg])
-    save_plot(random_graph, plot_caption)
+    if args.nodes:
+        total_nodes = args.nodes.split(",")
+    else:
+        total_nodes = ["100"]
+    for n_index, node_count_str in enumerate(total_nodes):
+        node_count = int(node_count_str)
+        random_graph, edge_probability = generate_random_graph(node_count)
+        plot_title = f"Generated an Erdős-Rényi graph with {node_count} nodes and edge probability {edge_probability}."
+        logger.info(plot_title)
+        sorted_cliques = generate_cliques(random_graph)
+        clique_count_msg = (
+            f"Found {len(sorted_cliques)} maximal cliques."
+            + save_statistics(sorted_cliques)
+        )
+        logger.info(clique_count_msg)
+        plot_caption = "\n".join([plot_title, clique_count_msg])
+        save_plot(random_graph, n_index, plot_caption)
--- cfa/cloudops/metaflow/examples/upload_files.py
+++ cfa/cloudops/metaflow/examples/upload_files.py
@@ -1,6 +1,35 @@
+import os
+import random
+import shutil
+import string
+
 from cfa.cloudops.auth import SPCredentialHandler
 from cfa.cloudops.blob_helpers import upload_files_in_folder
 from cfa.cloudops.client import get_blob_service_client
+
+
+def generate_random_string(length):
+    """Generates a random string of specified length using letters and digits."""
+    characters = string.ascii_letters + string.digits
+    random_string = "".join(random.choice(characters) for _ in range(length))
+    return random_string
+
+
+SUFFIX = generate_random_string(10)
+SOURCE_PATH = "cliques/"
+DESTINATION_PATH = f"cliques/{SUFFIX}/"
+os.makedirs(DESTINATION_PATH, exist_ok=True)
+
+
+def move_files_to_subfolder():
+    for item_name in os.listdir(SOURCE_PATH):
+        source_path = os.path.join(SOURCE_PATH, item_name)
+        destination_path = os.path.join(DESTINATION_PATH, item_name)
+        if os.path.isfile(source_path):
+            try:
+                shutil.move(source_path, destination_path)
+            except Exception:
+                continue
 
 
 def upload_artifacts():
@@ -15,4 +44,5 @@ def upload_artifacts():
 
 
 if __name__ == "__main__":
+    move_files_to_subfolder()
     upload_artifacts()
