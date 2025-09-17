17ffdba1230f37fe8bc99fcd18842482dc691197
Fawad Rafi <xop5@cdc.gov>
2025-09-17 02:44:52 +0000

    Updated container definition

--- cfa/cloudops/metaflow/examples/script.sh
+++ cfa/cloudops/metaflow/examples/script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-python -m cProfile -o /app/cliques/output.prof bk_algorithm.py -n $1
-python upload_files.py
+python -m cProfile -o /app/cliques/output.prof /app/bk_algorithm.py -n $1
+python /app/upload_files.py
