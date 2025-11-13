32ead895910d50e0cf4a42bd029081d088f4dbfa
Ryan Raasch <xng3@cdc.gov>
2025-11-13 20:07:39 +0000

    dag docs

--- docs/examples/dependencies/states.py
+++ docs/examples/dependencies/states.py
@@ -1,7 +1,9 @@
 import argparse
-import pandas as pd
-import random
 import os
+import random
+
+import pandas as pd
+
 
 def main(state: str) -> None:
     df = pd.read_csv("us_pop_by_state.csv")
@@ -9,16 +11,15 @@ def main(state: str) -> None:
     if not state_data.empty:
         population = state_data.iloc[0]["2020_census"]
         affected = int(population * random.uniform(0.01, 0.5))
-        output = pd.DataFrame({
-            "state": [state],
-            "population": [population],
-            "affected": [affected]
-        })
+        output = pd.DataFrame(
+            {"state": [state], "population": [population], "affected": [affected]}
+        )
         print(output)
         if os.path.exists("data") is False:
             os.mkdir("data")
         output.to_csv(f"data/{state}_affected.csv", index=False)
-    
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process state population data.")
     parser.add_argument("-s", "--state", type=str, help="State abbreviation to process")
